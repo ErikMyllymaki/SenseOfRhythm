71220180b2ef1d998ac5691e2338c68dccced118
ErikMyllymaki <erik.myl01@gmail.com>
2023-09-27 12:24:52 +0300

    rumpurytmi vihreestä napista

--- Assets/Scripts/RhythmPlayer.cs
+++ Assets/Scripts/RhythmPlayer.cs
@@ -6,14 +6,38 @@ public class RhythmPlayer : MonoBehaviour
 {
     public AudioClip drumSound; 
     private AudioSource audioSource; 
+    private bool isPlayingAudio = false; // Flag to track whether the audio is playing
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
     }
 
-    private void OnMouseDown() {
-        Debug.Log("painettu");
+    private void OnMouseDown()
+    {
+        if (!isPlayingAudio) // Check if the audio is not already playing
+        {
+            Debug.Log("Clicked");
+            if (drumSound != null && audioSource != null)
+            {
+                StartCoroutine(PlayAudio());
+            }
+            else
+            {
+                Debug.LogError("Audio clip or AudioSource component not set up properly.");
+            }
+        }
     }
 
+    private IEnumerator PlayAudio()
+    {
+        isPlayingAudio = true; // Set the flag to indicate that the audio is playing
+
+        audioSource.PlayOneShot(drumSound);
+
+        // Wait for the duration of the audio clip
+        yield return new WaitForSeconds(drumSound.length);
+
+        isPlayingAudio = false; // Reset the flag when the audio finishes
+    }
 }
