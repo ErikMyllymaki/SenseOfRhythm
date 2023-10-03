05c22ec0d732a1721993f3a2ab3176bc6b02c4c2
RikuHonkamaki <81312875+RikuHonkamaki@users.noreply.github.com>
2023-10-03 14:49:28 +0300

    ff vieläki rikki

--- Assets/Scripts/FFRumpu.cs
+++ Assets/Scripts/FFRumpu.cs
@@ -4,12 +4,10 @@ using UnityEngine.UI;
 public class FFrumpu : MonoBehaviour
 {
     public Text clickCountText;
-    public Button resetButton;
     public float gameDuration = 5.0f;
     public AudioClip clickSound; // Reference to your click sound
 
     public AudioSource audioSource;
-
 
     private int clickCount = 0;
     private float timer;
@@ -19,9 +17,6 @@ public class FFrumpu : MonoBehaviour
     {
         clickCountText.text = "Clicks: " + clickCount;
         timer = gameDuration;
-
-        // Add an onClick event to the reset button
-        resetButton.onClick.AddListener(ResetClickCount);
     }
 
     void Update()
@@ -61,11 +56,5 @@ public class FFrumpu : MonoBehaviour
             // Play the click sound
             audioSource.PlayOneShot(clickSound);
         }
-    }
-
-    public void ResetClickCount()
-    {
-        clickCount = 0;
-        clickCountText.text = "Clicks: " + clickCount; // Update the UI Text
     }
 }
