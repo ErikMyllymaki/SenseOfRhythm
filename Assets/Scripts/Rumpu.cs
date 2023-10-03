25058f3a969555848f17cf5a85858bc72e1437fa
RikuHonkamaki <81312875+RikuHonkamaki@users.noreply.github.com>
2023-10-03 12:55:28 +0300

    polyrhytm prefab

--- Assets/Scripts/Rumpu.cs
+++ Assets/Scripts/Rumpu.cs
@@ -8,7 +8,7 @@ public class Rumpu : MonoBehaviour
     public AudioClip clickSound;
     private AudioSource audioSource;
     private int currentBeatIndex = 0;
-    private float beatTolerance = 0.1f;
+    private float beatTolerance = 0.2f;
     private bool isFirstPress = false; 
     private float startTime;
 
